241f2a7b122a9e564578b1bd69ade5eafa219f53
Diego Vilhalva <diegorocha450@yahoo.com>
2025-09-22 14:06:17 -0300

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,22 +4,30 @@ import cookieParser from "cookie-parser"
 import "dotenv/config"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import path from "path"
 const app = express()
 
 const PORT = process.env.PORT || 4000
 
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
 
-app.get("/", (req, res) => {
-    return res.send("OK")
-})
+
+
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta: ${PORT}`)
