e95f99676aec642360669bb7522c52e2ed8c0952
Diego Vilhalva <diegorocha450@yahoo.com>
2025-09-22 13:37:08 -0300

    create message and auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import "dotenv/config"
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 const app = express()
 
 const PORT = process.env.PORT || 4000
@@ -12,11 +14,13 @@ app.use(cors())
 app.use(cookieParser())
 
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
     return res.send("OK")
 })
 
+app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Servidor rodando na porta: ${PORT}`)
 })
---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+
+
+const router = express.Router()
+
+router.post("/sign-up")
+
+export default router
