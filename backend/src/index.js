4ab2c97f3ac634822321b761f9fc900a8a0c8713
Diego Vilhalva <diegorocha450@yahoo.com>
2025-09-23 08:14:46 -0300

    update index

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
-if (ENV.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
   app.get("*", (_, res) => {
