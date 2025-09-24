cac10447870f9b94b576d95e6d959e7ba784181d
Diego Vilhalva <diegorocha450@yahoo.com>
2025-09-24 11:22:58 -0300

    welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
+import { sendWelcomeEmail } from "../emails/email.handler.js"
 import { generateToken } from "../lib/utils.js"
 import User from "../models/user.model.js"
 import { signupSchema } from "../validators/auth.validator.js"
 import bcrypt from "bcryptjs"
+import "dotenv/config"
 
 export const signup = async (req, res) => {
     try {
@@ -30,14 +32,20 @@ export const signup = async (req, res) => {
         })
 
         if (newUser) {
-            generateToken(newUser._id, res)
-            await newUser.save()
+            const savedUser = await newUser.save()
+            generateToken(savedUser._id, res)
             res.status(201).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
                 email: newUser.email,
                 profilePic: newUser.profilePic,
             });
+
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName,process.env.CLIENT_URL);
+            } catch (error) {
+                console.error("Erro ao enviar o email:", error);
+            }
         } else {
             res.status(400).json({ message: "Dados inválidos" })
         }
