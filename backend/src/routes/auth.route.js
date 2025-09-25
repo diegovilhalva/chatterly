ced39b04248252f2bf52727f08fe70516e9fb094
Diego Vilhalva <diegorocha450@yahoo.com>
2025-09-25 09:00:24 -0300

    implemt rate limiting

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from "express"
 import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js"
 import { protectRoute } from "../middleware/auth.middleware.js"
+import { arcjetProtection } from "../middleware/arcjet.middleware.js"
 
 
 const router = express.Router()
+
+router.use(arcjetProtection)
 
 router.post("/sign-up",signup)
 router.post("/login",login)
