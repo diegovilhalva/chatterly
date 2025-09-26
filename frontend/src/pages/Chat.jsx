76bfe2fbaf82fc3eb1a733324f9a10bf87bd180e
Diego Vilhalva <diegorocha450@yahoo.com>
2025-09-26 12:32:58 -0300

    create chat layout setup

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import ActiveTabSwitch from "../components/ActiveTabSwitch"
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer"
+import ChatContainer from "../components/ChatContainer"
+import ChatsList from "../components/ChatsList"
+import ContactList from "../components/ContactList"
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder"
+import ProfileHeader from "../components/ProfileHeader"
+import { useChatStore } from "../store/useChatStore"
 
 const Chat = () => {
+  const { activeTab,selectedUser } = useChatStore()
   return (
-    <div>Chat</div>
+    <div className='relative w-full max-w-6xl h-[800px]'>
+      <BorderAnimatedContainer>
+        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+          </div>
+        </div>
+        <div className="flex-1 flex-col bg-slate-900/50 backdrop-blur-sm ">
+          {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+        </div>
+      </BorderAnimatedContainer>
+    </div>
   )
 }
 
