2c2eee0a876025a66604969f1a152bc226c4fc8e
Diego Vilhalva <diegorocha450@yahoo.com>
2026-01-12 11:07:11 -0300

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,34 +4,28 @@ import cookieParser from "cookie-parser"
 import "dotenv/config"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-import path from "path"
 import { connectDB } from "./lib/db.js"
 import { app, server } from "./lib/socket.js"
 
 
 const PORT = process.env.PORT || 4000
 
-const __dirname = path.resolve()
 
 app.use(express.json({limit:"5mb"}))
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(cookieParser())
 
 
+app.get("/", (req, res) => {
+    res.send("Servidor ok")
+})
 
 
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get(/.*/, (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    });
-
-}
 
 
 server.listen(PORT, () => {
